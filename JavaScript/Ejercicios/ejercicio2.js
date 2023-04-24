5ff6e75835818866588b30161ef6b9d2f7272c54
Yedpt <115632076+Yedpt@users.noreply.github.com>
2023-04-24 17:02:43 +0200

    Add files via upload

---
+++ JavaScript/Ejercicios/ejercicio2.js
@@ -0,0 +1,12 @@
+const lista = [
+    "Yeder",
+    30,
+    true,
+    new Date(1993, 02, 18),
+    { 
+        titulo: "Epidemia",
+        autor: "Robin cook",
+        fecha: new Date(2003, 02, 11),
+        url: "https://www.amazon.es/Epidemia-BEST-SELLER-ROBIN-COOK/dp/8497593057"
+      },
+  ];
