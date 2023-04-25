edf7ac6613ef5d9bef5cbb6031735b8e7488d32c
Yedpt <115632076+Yedpt@users.noreply.github.com>
2023-04-25 12:37:09 +0200

    Add files via upload

---
+++ JavaScript/Ejercicios/ejercicio3-for.js
@@ -0,0 +1,5 @@
+let factorial = 10;
+for (let i = factorial-1; i >= 1; i--) {
+    factorial = factorial * i;
+    console.log(factorial);
+  }
---
+++ JavaScript/Ejercicios/ejercicio3-while.js
@@ -0,0 +1,8 @@
+let factorial=10;
+factcont=factorial-1
+cont=1;
+while (cont<=factcont) {
+  factorial=factorial*cont;
+  cont++; //4
+  console.log(factorial);
+}
