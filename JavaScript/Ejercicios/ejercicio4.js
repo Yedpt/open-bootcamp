03c873b8fd3991ec2ca8b107367bab9f2b871e2c
Yedpt <115632076+Yedpt@users.noreply.github.com>
2023-04-26 13:41:07 +0200

    Add files via upload

---
+++ JavaScript/Ejercicios/ejercicio4.js
@@ -0,0 +1,21 @@
+let nombre = "Hola, me llamo Yeder";
+
+let apellido = "Mi apellido es Pimentel";
+
+let estudiante = nombre.concat(" ").concat(apellido);
+
+let estudianteMayus = estudiante.toUpperCase();
+
+let estudianteMinus = estudiante.toLowerCase();
+
+let contEstudiante = estudiante.length;
+
+let firstLetterNombre = nombre.substring(0,1)
+
+let LastLetterApellido = apellido.substring(apellido.length - 1, apellido.length);
+
+let estudianteSinEspacios = estudiante.replace(/ /g, "");
+
+let nombreEnestudiantes = estudiante.includes(nombre);
+
+console.log(nombreEnestudiantes);
