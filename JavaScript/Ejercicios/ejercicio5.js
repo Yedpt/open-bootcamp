c6d9f261a4ed1a87d2fd3e80763e6d632eb1f977
Yedpt <115632076+Yedpt@users.noreply.github.com>
2023-04-27 13:37:15 +0200

    Add files via upload

---
+++ JavaScript/Ejercicios/ejercicio5.js
@@ -0,0 +1,12 @@
+let alturaEnCm = 180;
+let alturaEnMt = 1.80;
+let pesoKl =  102.10 ;
+let alturaEnMtA = 1.80;
+let pesoKlB = 102.10;
+let sonIguales ;
+
+console.log(Math.ceil(alturaEnMtA)); //redonde hacia arriba
+
+console.log(Math.floor(pesoKlB));//redondea hacia abajo
+
+console.log(Number.MAX_VALUE + 1 === Number.MAX_VALUE);
