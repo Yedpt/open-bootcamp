30499fe84917f52c931606d7f522e53c4dd99c67
Yedpt <115632076+Yedpt@users.noreply.github.com>
2023-05-18 14:42:13 +0200

    Add files via upload

---
+++ JavaScript/Ejercicios/ejercicio10/index.js
@@ -0,0 +1,6 @@
+import * as ModuloEjercicio from './controller.js';
+import chalk from 'chalk';
+
+// console.log(ModuloEjercicio.multiplica(ModuloEjercicio.suma(1,2), ModuloEjercicio.suma(4,5)));
+
+console.log(chalk.green(ModuloEjercicio.multiplica(ModuloEjercicio.suma(1,2), ModuloEjercicio.suma(4,5))));
